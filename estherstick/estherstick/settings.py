1b95a206cc77282ce01eb2a3b60471987d9ee8fa
Zao0oza <sefeigin@mail.ru>
2021-09-26 19:33:36 +0300

    17 lesson

--- estherstick/estherstick/settings.py
+++ estherstick/estherstick/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 from os import path
+from .key import key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ok^h&#le*9$0wa(%k&wws^*5w+w9m(39v###lg+6@9(1nlx326'
+SECRET_KEY = key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- estherstick/news/models.py
+++ estherstick/news/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.urls import reverse
 
-
 class News(models.Model):
     title = models.CharField(max_length=150, verbose_name='Наименование')
     content = models.TextField(blank=True, verbose_name='Контент')
